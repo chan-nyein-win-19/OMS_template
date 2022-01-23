ebacd3ca2cc96d8bfe5d9fd1f7937aa637e61c0b
GICM-PC150\GICM <channyeinwin@gicjp.com>
2022-01-23 19:33:47 +0630

    no message

--- OMS/app/Http/Controllers/OMSControllers/EmailSendController.php
+++ OMS/app/Http/Controllers/OMSControllers/EmailSendController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\OMSControllers;
 
 use App\Http\Controllers\Controller;
-namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use Auth;
@@ -15,13 +14,15 @@ use App\Models\Otp;
 
 class EmailSendController extends Controller
 {
+    //
     public function forgotpwd()
     {
         return view('login.forgotpwd');
     }
 
-    function checkemail(Request $request)
-    {
+
+   function checkemail(Request $request)
+   {
         $this->validate($request,[
            'email'=>'required|string|email'  
         ]);
@@ -36,42 +37,29 @@ class EmailSendController extends Controller
                 'body'=>'If you forgot your password to login,Please use this OTP,and then set new password.
                     Your One Time Password : '. $number
             ];
-            $this->validate($request,[
-                'email'=>'required|string|email'  
+    
+        Mail::to($user)->send(new ForgotpwdEmail($details));
+
+        $otp=Otp::where('employeeid',$user->id)->first();
+
+        if($otp == NULL)
+        {
+            $otp1 = new Otp;
+            $otp1->employeeid=$user->id;
+            $otp1->otp=$number;
+    
+            $otp1->save();
+        }
+        else{
+            Otp::where('id',$otp->id)->update([
+               
+                'employeeid'=>$user->id,
+                'otp'=>$number,
             ]);
-
-            $user = User::where('email',$request->get('email'))->first();
-       
-            if($user != NULL)
-            {    
-                $number = mt_rand(100000, 999999);
-                $details = [
-                    'title'=> 'One Time Password from Office Management System',
-                    'body'=>'If you forgot your password to login,Please use this OTP,and then set new password.
-                    Your One Time Password : '.$number
-                ];
-    
-            Mail::to($user)->send(new ForgotpwdEmail($details));
-
-            $otp=Otp::where('employeeid',$user->id)->first();
-
-            if($otp == NULL)
-            {
-                $otp1 = new Otp;
-                $otp1->employeeid=$user->id;
-                $otp1->otp=$number;
-    
-                $otp1->save();
-            }
-            else{
-                Otp::where('id',$otp->id)->update([
-                    'employeeid'=>$user->id,
-                    'otp'=>$number,
-                ]);
-            }
+        }
         
-            $employeeid = $user->id;
-            return view('login.otpform',compact('employeeid'));
+        $employeeid = $user->id;
+        return view('login.otpform',compact('employeeid'));
 
         }else
         {
@@ -82,6 +70,5 @@ class EmailSendController extends Controller
     function otpform()
     {
      return view('login.otpform');
-    }
     }
 }
