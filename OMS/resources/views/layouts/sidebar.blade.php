5079de1c63074f788d7c0c6a847e8f64585aabb2
abk666 <aungbhonekw@gmail.com>
2022-01-24 13:19:42 +0630

    Adding sidebar

--- OMS/resources/views/layouts/sidebar.blade.php
+++ OMS/resources/views/layouts/sidebar.blade.php
@@ -16,6 +16,31 @@
                 <i class="metismenu-state-icon caret-left"></i>
             </a>
         </li>
+        <li class="app-sidebar__heading">Leaves</li>
+        <li>
+                <!-- <a href="{{url("/leaveRequestForm")}}" class="textstyle">
+                        Leave Request
+                </a> -->
+                <a href="{{route('leaves.index')}}" class="textstyle">
+                        Leave Request
+                </a>
+        </li>
+        <li>
+                <!-- <a href="{{url("/leaveRecord")}}" class="textstyle">
+                        <i class="metismenu-icon">
+        </i>Leave Record
+                </a> -->
+                <a href="{{route('leaves.show')}}" class="textstyle">
+                        <i class="metismenu-icon">
+        </i>Leave Record
+                </a>
+        </li>
+        <li>
+                <a href="{{url("/leader/leaveRecord")}}" class="textstyle">
+                        <i class="metismenu-icon">
+                </i>Leader Leave
+                 </a>
+                </li>
         <li class="app-sidebar__heading">Assets</li>
         <li>
             <a href="#">
