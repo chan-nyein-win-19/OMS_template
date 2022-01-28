8f44908a8e0c992b9ff3317f7463ac83f6be98ae
abk666 <aungbhonekw@gmail.com>
2022-01-28 14:55:34 +0630

    adding attandance

--- OMS/resources/views/leave/leaveEdit.blade.php
+++ OMS/resources/views/leave/leaveEdit.blade.php
@@ -30,8 +30,9 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">EmployeeId*</label>
                         <div class="col-sm-10">
-                            <input type="number" class="form-control" id="employeeId" name="employeeId"
+                            <input type="number" class="form-control" id="employeeId"
                                 value="{{auth()->user()->employeeid}}" readonly>
+                                <input type="hidden" value="{{auth()->user()->id}}" name="employeeId">
                         </div>
                     </div>
                     @error('employeeId')
