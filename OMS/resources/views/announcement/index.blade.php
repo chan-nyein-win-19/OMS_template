d228e9e163517d9f9ca021ec355d9f6b3272d5a7
Chan Nyein Win <channyeinwin.cnw@gmail.com>
2022-02-02 22:34:24 +0630

    announcement and user list fix

--- OMS/resources/views/announcement/index.blade.php
+++ OMS/resources/views/announcement/index.blade.php
@@ -32,26 +32,26 @@
             </thead>
             <tbody>
                 @foreach ($list as $item)
-                <tr>
-                    <td>
-                        {{ $item->title }}
-                    </td>
-                    <td>
-                        {{ $item->content }}
-                    </td>                        
-                    <td>   
-                        <a href="{{ route('announcements.edit', $item->id) }}" class="mb-2 mr-2 btn-transition btn btn-outline-primary">
-                            <i class="fa fa-fw"></i>
-                        </a>
-                        <form method="POST" action="{{route('announcements.destroy',['announcement'=>$item])}}" id="form{{$item->id}}">
-                            @csrf
-                            <input name="_method" type="hidden" value="DELETE">
-                            <button type="button" onclick=deleteRecord(this.id) class="mb-2 mr-2 btn-transition btn btn-outline-danger" data-toggle="tooltip" title="Delete" id="{{$item->id}}">
-                                <i class="fa fa-fw"></i>
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            {{ $item->title }}
+                        </td>
+                        <td>
+                            {{ $item->content }}
+                        </td>                        
+                        <td>   
+                            <a href="{{ route('announcements.edit', $item->id) }}" class="mb-2 mr-2 btn-transition btn btn-outline-primary" data-toggle="tooltip" title='Edit'>
+                                <i class="fa fa-fw"></i>
+                            </a>
+                            <form method="POST" action="{{ route('announcements.destroy',['announcement'=>$item]) }}" id="form{{ $item->id }}">
+                                @csrf
+                                <input name="_method" type="hidden" value="DELETE">
+                                <button type="button" onclick=deleteRecord(this.id) class="mb-2 mr-2 btn-transition btn btn-outline-danger" data-toggle="tooltip" title="Delete" id="{{$item->id}}">
+                                    <i class="fa fa-fw"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
@@ -67,76 +67,56 @@
     <script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.js') }}"></script>
     <script src="{{ asset('/storage/OMS/bootstrap5/popper.min.js') }}"></script>
 
-<script type="text/javascript">
-    jQuery(function($) {
+    <script type="text/javascript">
+        jQuery(function($) {
         //initiate dataTables plugin
         var myTable =
-            $('#table')
-            //.wrap("<div class='dataTables_borderWrap' />")   //if you are applying horizontal scrolling (sScrollX)
-            .DataTable({
-                bAutoWidth: false,
-                "aoColumns": [
-                    null,
-                    null,
-                    null          
-
-                ],
-                "aaSorting": [],
-
-                //"bProcessing": true,
-                //"bServerSide": true,
-                //"sAjaxSource": "http://127.0.0.1/table.php"   ,
-
-                //,
-                //"sScrollY": "200px",
-                //"bPaginate": false,
-
-                //"sScrollX": "100%",
-                //"sScrollXInner": "120%",
-                //"bScrollCollapse": true,
-                //Note: if you are applying horizontal scrolling (sScrollX) on a ".table-bordered"
-                //you may want to wrap the table inside a "div.dataTables_borderWrap" element
-
-                //"iDisplayLength": 50    
-
+        $('#table')
+        .DataTable({
+            bAutoWidth: false,
+            "aoColumns": [
+                null,
+                null,
+                null 
+            ],
+            "aaSorting": [],
                 select: {
                     style: 'multi'
                 }
             });
-    });
-</script>
-
-<script type="text/javascript">
-    function deleteRecord($id) {
-        bootbox.confirm({
-            message: "Do You Really want to delete it?This can't be undone.",
-            buttons: {
-                confirm: {
-                    label: 'Yes',
-                    className: 'btn-success'
-                },
-                cancel: {
-                    label: 'No',
-                    className: 'btn-danger'
-                }
-            },
-            callback: function(result) {
-                if (result) {
-                    let formToDelete = document.getElementById("form"+$id);
-                    formToDelete.submit();
-                    bootbox.alert({
-                        message: "Successfully Deleted!",
-                        callback: function() {
-                            console.log('This was logged in the callback!');
-                        }
-                    })
-                }
-            }
         });
-    }
-    setTimeout(() => {
-        $('.alert-success').addClass('d-none');
-    }, 3000);
-</script>
+    </script>
 
+    <script type="text/javascript">
+        function deleteRecord($id) {
+            bootbox.confirm({
+                message: "Do You Really want to delete it?This can't be undone.",
+                buttons: {
+                    confirm: {
+                        label: 'Yes',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'No',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function(result) {
+                    if (result) {
+                        let formToDelete = document.getElementById("form"+$id);
+                        formToDelete.submit();
+                        bootbox.alert({
+                            message: "Successfully Deleted!",
+                            callback: function() {
+                                console.log('This was logged in the callback!');
+                            }
+                        })
+                    }
+                }
+            });
+        }
+        setTimeout(() => {
+            $('.alert-success').addClass('d-none');
+        }, 3000);
+    </script>
 @endsection
