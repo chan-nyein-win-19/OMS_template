180d2ddd889691740aa25afaaa55285f648d17ae
GICM-PC150\GICM <channyeinwin@gicjp.com>
2022-02-10 10:04:34 +0630

    brand fix

--- OMS/resources/views/brand/edit.blade.php
+++ OMS/resources/views/brand/edit.blade.php
@@ -17,31 +17,26 @@
 @section('content')
     <div class="main-card mb-3 card">
         <div class="card-body"><h5 class="card-title">Brand Update Form</h5>
-        	<br>
+            <br>
             <form action="{{ route('brands.update',[$edit->id]) }}" method="post" >
                 @csrf
                 @method('PUT')
                 <div class="position-relative row form-group">
-                        <label for="name" class="col-sm-2 col-form-label">Name<span style="color: red">*</span></label>
-                        
+                    <label class="col-sm-2 col-form-label">Name<span style="color: red">*</span></label>
                         <div class="col-sm-10">
-                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') ? old('name') : $edit->name }}" placeholder="Please Enter Brand Name"  />
-
+                            <input type="text" class="form-control" name="name" value="{{ old('name') ? old('name') : $edit->name }}" placeholder="Please Enter Brand Name"/>
                             @error("name")
-                            <span class="text-danger"> {{ $errors->first('name') }} </span>
-                            @enderror  
-
+                                <span class="text-danger"> {{ $errors->first('name') }} </span>
+                            @enderror
                         </div>
-
                     </div>
-                
                     <div class="position-relative row form-group">
-                        <label for="content" class="col-sm-2 col-form-label">Description<span style="color: red">*</span></label>
+                        <label class="col-sm-2 col-form-label">Description<span style="color: red">*</span></label>
                         <div class="col-sm-10">
-                            <textarea id="description" type="textarea" class="form-control @error('description') @enderror" rows="3" name="description" placeholder="Please Enter Description" >{{ old('description') ? old('description') : $edit->description }}</textarea>
-                            <span class="text-danger"> 
-                                {{ $errors->first('description') }} 
-                            </span>
+                            <textarea class="form-control" rows="3" name="description" placeholder="Please Enter Description" >{{ old('description') ? old('description') : $edit->description }}</textarea>
+                            @error('description')
+                                <span class="text-danger"> {{ $errors->first('description') }} </span>
+                            @enderror    
                         </div>
                     </div> 
                 <div class="text-center">
@@ -56,13 +51,4 @@
 @section('script')
     <script src="{{ asset('/storage/OMS/login/jquery.min.js') }}"></script>
     <script src="{{ asset('/storage/OMS/login/bootstrap.min.js') }}"></script>
-    <script type="text/javascript">
-        $(document).ready(()=>{
-            @if ($errors->first('title'))
-                $("input[name='name']").focus();
-            @elseif($errors->first('content')) 
-                $("textarea[name='description']").focus();
-            @endif
-        });
-    </script>
 @endsection
