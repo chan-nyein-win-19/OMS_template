42188fc8c90e8a94aff0732587aec41978a8bfec
Chan Nyein Win <channyeinwin.cnw@gmail.com>
2022-02-10 17:25:03 +0630

    change cdn link to local link(bootstrap.bundle.min.js)

--- OMS/resources/views/SubCategory/index.blade.php
+++ OMS/resources/views/SubCategory/index.blade.php
@@ -124,7 +124,7 @@
     <script src="{{ asset('/storage/OMS/bootbox/bootbox.locale.js') }}"></script>
     <script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.js') }}"></script>
     <script src="{{ asset('/storage/OMS/bootstrap5/popper.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.bundle.min.js') }}"></script>
     <script>
         $(document).ready(function() {
             $('#table').DataTable();
