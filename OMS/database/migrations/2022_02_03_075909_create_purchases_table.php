f1af73ae09de7a2936df0a3d3c97e423880505bb
DESKTOP-L6TLDMF\PC090 <yeluck17>
2022-02-11 18:33:24 +0630

    itemcode

--- OMS/database/migrations/2022_02_03_075909_create_purchases_table.php
+++ OMS/database/migrations/2022_02_03_075909_create_purchases_table.php
@@ -16,7 +16,6 @@ class CreatePurchasesTable extends Migration
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
             $table->date('date');
-            $table->string('itemcode');
             $table->string('condition');
             $table->integer('quantity');
             $table->double('totalprice');
