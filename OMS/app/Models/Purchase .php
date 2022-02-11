7b443d395ff2aaddf4359658ef8ca3dbc284c288
DESKTOP-L6TLDMF\PC090 <yeluck17>
2022-02-11 16:18:15 +0630

    initial commit

--- OMS/app/Models/Purchase .php
+++ OMS/app/Models/Purchase .php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class subCategory extends Model
+class Purchase extends Model
 {
     use HasFactory;
-    
-    protected $table='sub_categories';
 }
