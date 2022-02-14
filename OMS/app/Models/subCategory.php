935c9be3b6d24b9f4625dd7275ce7bd69012f38d
Chan Nyein Win <channyeinwin.cnw@gmail.com>
2022-02-14 17:35:41 +0630

    no message

--- OMS/app/Models/subCategory.php
+++ OMS/app/Models/subCategory.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class subCategory extends Model
 {
     use HasFactory;
-    
-    protected $table='sub_categories';
+
+    protected $fillable = [
+        'name',
+        'categoryId',
+        'description'
+
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class,'categoryId');
+    }
 }
