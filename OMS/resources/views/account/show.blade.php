35c57c76f83df4820e405694001b4efc0f49b86d
Chan Nyein Win <channyeinwin.cnw@gmail.com>
2022-02-16 16:37:45 +0630

    Account Info Design Change

--- OMS/resources/views/account/show.blade.php
+++ OMS/resources/views/account/show.blade.php
@@ -1,78 +1,61 @@
 @extends('layouts.app')
 
 @section("content")
- 
-  
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header justify-content-center" style="font-size:20px;">{{ __('Account Information') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="">
-                        @csrf
-                        <div class="row mb-3">
-                            <label for="employeeid" class="col-md-4 col-4 col-form-label text-md-end " style="font-weight:bold;">{{ __('Employee ID :') }}</label>
-                            <label class="col-md-6 col-6 col-form-label"> {{ $user->employeeid}}<label>
-                            
-                        </div>
-							
-                        <div class="row mb-3 ">
-						 <label for="fname" class="col-md-4 col-4 col-form-label text-md-end" style="font-weight:bold;">{{ __('FirstName :') }}</label>
-
-                            <label class="col-md-6 col-6 col-form-label ">
-                            {{ $user->fname}}
-                            </label>
-                        </div>
-
-
-                        <div class="row mb-3">
-                            <label for="lname" class="col-md-4 col-4 col-form-label text-md-end" style="font-weight:bold;">{{ __('LastName :') }}</label>
-
-                            <label class="col-md-6 col-6 col-form-label">
-                            {{ $user->lname }}
-                            </label>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-4 col-form-label text-md-end" style="font-weight:bold;">{{ __('Username :') }}</label>
-
-                            <label class="col-md-6 col-6 col-form-label">
-                            {{ $user->username }}
-                            </label>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-4 col-form-label text-md-end" style="font-weight:bold;">{{ __('E-Mail :') }}</label>
-
-                            <label class="col-md-6 col-6 col-form-label ">
-                            {{ $user->email}}
-                            </label>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="role" class="col-md-4 col-4 col-form-label text-md-end" style="font-weight:bold;">{{ __('Role :') }}</label>
-
-                            <label class="col-md-6 col-6 col-form-label">
-                            {{ $user->role}}
-                            </label>
-                        </div>
-
-                   		 <hr>
-
+                <div class="card-header justify-content-center"  style="font-size:20px;">{{ __('Account Information') }}</div>
+                    <div class="card-body">
+                        <form method="POST" action=""> 
+                            @csrf
+                            @method('PUT')
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('Employee ID') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="employeeid" value="{{ $user->employeeid }}" disabled>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('FirstName') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="fname" value="{{ $user->fname }}" disabled>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('LastName') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="lname" value="{{ $user->lname }}" disabled>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('Username') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="username" value="{{ $user->username }}" disabled>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('E-Mail') }}</label>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" name="email" value="{{ $user->email }}" disabled>
+                                </div>
+                            </div>
+                            <div class="row mb-3">
+                                <label class="col-md-4 col-form-label text-md-end font-weight-bold">{{ __('Role') }}</label>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" name="role" value="{{ $user->role }}" disabled>
+                                </div>
+                            </div>
+                            <hr>
                             <div class="text-center" >            
                                 <a class="mb-2 mr-2 btn btn-primary" style="font-size: 14px;" href="{{url('/accounts/'.$user->id.'/edit')}}">{{ __('Edit') }}</a>
 				 				&nbsp;&nbsp;
 					            <a class="mb-2 mr-2 btn btn-danger" style="font-size: 14px;" href="{{url('/successlogin')}}"> {{ __('Cancel') }}</a>
                             </div>
-
-
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-  
-
 @endsection
+
