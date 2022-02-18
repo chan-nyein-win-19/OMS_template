85bc82f6da012df4d8dbd5579d013f71fbfb2c61
GICM-PC150\GICM <channyeinwin@gicjp.com>
2022-02-18 11:02:18 +0630

    purchase fix

--- OMS/resources/views/purchase/index.blade.php
+++ OMS/resources/views/purchase/index.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
-@section('title','announcement list')
+@section('title','purchase list')
 
 @section('style')
-<link rel="stylesheet" href="{{ asset('/storage/OMS/data-tables/jquery.dataTables.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/storage/OMS/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/storage/OMS/data-tables/jquery.dataTables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/storage/OMS/css/style.css') }}">
 @endsection
 
 @section('topbar')
-@parent
+    @parent
 @endsection
 
 @section('sidebar')
-@parent
+    @parent
 @endsection
 
 @section('content')
@@ -35,7 +35,6 @@
                     <th>Category</th>
                     <th>SubCategory</th>
                     <th>Brand</th>
-                   
                     <th>Action</th>
                 </tr>
             </thead>
@@ -68,12 +67,11 @@
                     </td> 
                     <td>
                         {{ $item->brandid }}
-                    </td>       
-                                     
+                    </td>            
                     <td>     
-                    <a href="{{ route('otherpurchase.edit', $item->id) }}" class="mb-2 mr-2 btn-transition btn btn-outline-primary" data-toggle="tooltip" title='Edit'>
-                    <i class="fa fa-fw"></i>
-                    </a>
+                        <a href="{{ route('otherpurchase.edit', $item->id) }}" class="mb-2 mr-2 btn-transition btn btn-outline-primary" data-toggle="tooltip" title='Edit'>
+                            <i class="fa fa-fw"></i>
+                        </a>
                         <form method="POST" action="{{route('otherpurchase.destroy',['otherpurchase'=>$item])}}" id="form{{$item->id}}">
                             @csrf
                             <input name="_method" type="hidden" value="DELETE">
@@ -90,92 +88,90 @@
 @endsection
 
 @section('script')
+    <script src="{{ asset('/storage/OMS/data-tables/jquery.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/data-tables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootbox/bootbox.all.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootbox/bootbox.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootbox/bootbox.locale.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootstrap5/popper.min.js') }}"></script>
+    <script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.bundle.min.js') }}"></script>
 
-<script src="{{ asset('/storage/OMS/data-tables/jquery.js') }}"></script>
-<script src="{{ asset('/storage/OMS/data-tables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootbox/bootbox.all.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootbox/bootbox.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootbox/bootbox.locale.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.min.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootstrap5/popper.min.js') }}"></script>
-<script src="{{ asset('/storage/OMS/bootstrap5/bootstrap.bundle.min.js') }}"></script>
+    <script type="text/javascript">
+        jQuery(function($) {
+            //initiate dataTables plugin
+            var myTable =
+                $('#table')
+                //.wrap("<div class='dataTables_borderWrap' />")   //if you are applying horizontal scrolling (sScrollX)
+                .DataTable({
+                    bAutoWidth: false,
+                    "aoColumns": [
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                            
 
-<script type="text/javascript">
-jQuery(function($) {
-    //initiate dataTables plugin
-    var myTable =
-        $('#table')
-        //.wrap("<div class='dataTables_borderWrap' />")   //if you are applying horizontal scrolling (sScrollX)
-        .DataTable({
-            bAutoWidth: false,
-            "aoColumns": [
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                null,
-                       
+                    ],
+                    "aaSorting": [],
 
-            ],
-            "aaSorting": [],
+                    //"bProcessing": true,
+                    //"bServerSide": true,
+                    //"sAjaxSource": "http://127.0.0.1/table.php"   ,
 
-            //"bProcessing": true,
-            //"bServerSide": true,
-            //"sAjaxSource": "http://127.0.0.1/table.php"   ,
+                    //,
+                    //"sScrollY": "200px",
+                    //"bPaginate": false,
 
-            //,
-            //"sScrollY": "200px",
-            //"bPaginate": false,
+                    //"sScrollX": "100%",
+                    //"sScrollXInner": "120%",
+                    //"bScrollCollapse": true,
+                    //Note: if you are applying horizontal scrolling (sScrollX) on a ".table-bordered"
+                    //you may want to wrap the table inside a "div.dataTables_borderWrap" element
 
-            //"sScrollX": "100%",
-            //"sScrollXInner": "120%",
-            //"bScrollCollapse": true,
-            //Note: if you are applying horizontal scrolling (sScrollX) on a ".table-bordered"
-            //you may want to wrap the table inside a "div.dataTables_borderWrap" element
+                    //"iDisplayLength": 50    
 
-            //"iDisplayLength": 50    
-
-            select: {
-                style: 'multi'
-            }
-        });
-});
-</script>
-
-<script type="text/javascript">
-function deleteRecord($id) {
-    bootbox.confirm({
-        message: "Do You Really want to delete it?This can't be undone.",
-        buttons: {
-            confirm: {
-                label: 'Yes',
-                className: 'btn-success'
-            },
-            cancel: {
-                label: 'No',
-                className: 'btn-danger'
-            }
-        },
-        callback: function(result) {
-            if (result) {
-                let formToDelete = document.getElementById("form"+$id);
-                formToDelete.submit();
-                bootbox.alert({
-                    message: "Successfully Deleted!",
-                    callback: function() {
-                        console.log('This was logged in the callback!');
+                    select: {
+                        style: 'multi'
                     }
-                })
-            }
-        }
-    });
-}
+                });
+        });
+    </script>
 
-</script>
+    <script type="text/javascript">
+        function deleteRecord($id) {
+            bootbox.confirm({
+                message: "Do You Really want to delete it?This can't be undone.",
+                buttons: {
+                    confirm: {
+                        label: 'Yes',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'No',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function(result) {
+                    if (result) {
+                        let formToDelete = document.getElementById("form"+$id);
+                        formToDelete.submit();
+                        bootbox.alert({
+                            message: "Successfully Deleted!",
+                            callback: function() {
+                                console.log('This was logged in the callback!');
+                            }
+                        })
+                    }
+                }
+            });
+        }
+    </script>
 
 @endsection
