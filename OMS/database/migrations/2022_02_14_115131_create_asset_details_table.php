a2b874a3926a4f0c20a00816154c3a332abd9f6c
abk666 <aungbhonekw@gmail.com>
2022-02-18 13:48:45 +0630

    asset details

---
+++ OMS/database/migrations/2022_02_14_115131_create_asset_details_table.php
@@ -0,0 +1,53 @@
+<?php
+
+
+
+use Illuminate\Database\Migrations\Migration;
+
+use Illuminate\Database\Schema\Blueprint;
+
+use Illuminate\Support\Facades\Schema;
+
+
+
+class CreateAssetDetailsTable extends Migration
+
+{
+
+    /**
+
+     * Run the migrations.
+
+     *
+
+     * @return void
+
+     */
+
+    public function up()
+
+    {
+        Schema::create('asset_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('itemCode');
+            $table->string('condition');
+            $table->double('currentPrice');
+            $table->integer('purchaseId');
+            $table->timestamps();
+        });
+
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asset_details');
+    }
+
+}
