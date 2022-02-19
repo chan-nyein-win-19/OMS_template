49cb88faf1e42a43d1e6840914d5da6fc93d6bbe
Chan Nyein Win <channyeinwin.cnw@gmail.com>
2022-02-19 15:19:31 +0630

    PC update commit

--- OMS/app/Http/Controllers/OMSControllers/PcController.php
+++ OMS/app/Http/Controllers/OMSControllers/PcController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\OMSControllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\PC;
+use App\Models\Category;
+use App\Models\Brand;
+use App\Models\subCategory;
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class PcController extends Controller
@@ -61,9 +65,14 @@ class PcController extends Controller
      * @param  \App\Models\PC  $pC
      * @return \Illuminate\Http\Response
      */
-    public function edit(PC $pC)
+    public function edit($id)
     {
-        //
+        $edit=Pc::find($id);
+        $category=Category::all();
+        $brand=Brand::all();
+        $subCategory=subCategory::all();
+        $purchase=Purchase::all();
+        return view('pc.edit',compact(['edit','brand','category','subCategory', 'purchase']));                
     }
 
     /**
@@ -73,11 +82,42 @@ class PcController extends Controller
      * @param  \App\Models\PC  $pC
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, PC $pC)
+    public function update(Request $request, $id)
     {
-        //
-    }
+        //dd('update fun');
+        $validator=validator(request()->all(),[         
+            'category'=>'required',
+            'subcategory'=>'required',
+            'brand'=>'required',
+            'cpu'=>'required',
+            'ram'=>'required',
+            'storage'=>'required',
+            'itemcode'=>'required',
+            'model'=>'required',
+            'condition'=>'required',
+            'currentprice'=>'required',
+        ]);
+    
+        if($validator->fails()) {
+            return back()->withErrors($validator);
+        }        
 
+        $pc = Pc::find($id);
+        $pc->cpu=request()->cpu;
+        $pc->ram=request()->ram;
+        $pc->storage=request()->storage;
+        $pc->model=request()->model;
+        $pc->itemcode=request()->itemcode;
+        $pc->condition=request()->condition;
+        $pc->currentprice=request()->currentprice;
+        $pc->categoryid=request()->category;
+        $pc->subcategoryid=request()->subcategory;
+        $pc->brandid=request()->brand;
+        $pc->save();
+    
+        return redirect("pc");       
+    
+    }
     /**
      * Remove the specified resource from storage.
      *
